ebf13fbab53e634196d84d38fc116dbe668bc87c
Justin M <justinmcduffey@gmail.com>
2026-05-21 23:41:34 -0500

    LLM Generator Start

---
+++ src/tracks/levelGeneration/languageModelLevelGenerator/LevelGenerator.java
@@ -0,0 +1,13 @@
+package tracks.levelGeneration.languageModelLevelGenerator;
+
+import core.game.GameDescription;
+import core.generator.AbstractLevelGenerator;
+import tools.ElapsedCpuTimer;
+
+public class LevelGenerator extends AbstractLevelGenerator{
+
+    public String generateLevel(GameDescription game, ElapsedCpuTimer elapsedTimer){
+        return "";
+    }
+    
+}
