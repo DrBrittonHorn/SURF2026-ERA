d33a47847947a58b8d2e22a6a049140535863655
Justin M <justinmcduffey@gmail.com>
2026-05-31 22:27:06 -0500

    Updates to metric pipeline, more locally generated levels

--- src/tracks/metricCalculation/calculateMetrics.java
+++ src/tracks/metricCalculation/calculateMetrics.java
@@ -3,6 +3,7 @@ package tracks.metricCalculation;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 import tools.com.google.gson.JsonObject;
 import tracks.metricCalculation.metrics.Density;
@@ -10,7 +11,7 @@ import tracks.metricCalculation.metrics.NegativeSpace;
 
 public class calculateMetrics {
 
-    public static String createLevelMetricJson(String levelText){
+    public static JsonObject createLevelMetricJson(String levelText){
         JsonObject levelMetrics = new JsonObject();
         
         // Individual Metrics added to the json object below
@@ -18,12 +19,41 @@ public class calculateMetrics {
         levelMetrics.addProperty("NegativeSpace", NegativeSpace.calculateNegativeSpace(levelText));
         //levelMetrics.addProperty("SomeMetric", SomeMetric.calculateSomeMetric(levelText));
 
-        return levelMetrics.toString();
+        return levelMetrics;
+    }
+
+    public static JsonObject createFolderMetricJson(String levelFolderPath){
+        JsonObject jsonObject = new JsonObject();
+        try {
+            
+            Stream<Path> stream = Files.walk(Path.of(levelFolderPath));
+            stream.forEach(f -> {
+                if (f.toString().endsWith(".txt")){
+                    try {
+                        jsonObject.add(f.toString(), createLevelMetricJson(Files.readString(f)));
+                        
+                    } catch (IOException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
+                }
+            });
+            return jsonObject;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return jsonObject;
     }
 
 
     public static void main(String[] args) throws IOException{
         String testLevel = Files.readString(Path.of("generatedExamples/constructiveLevelGenerator/aliens/aliens_lvl001.txt"));
-        System.out.println(createLevelMetricJson(testLevel));
+        //System.out.println(createLevelMetricJson(testLevel));
+
+        String testFolder = "generatedExamples/constructiveLevelGenerator";
+
+        System.out.println(createFolderMetricJson(testFolder));
+        Files.writeString(Path.of(testFolder + "/" + "metrics.json"), createFolderMetricJson(testFolder).toString());
     }
 }
