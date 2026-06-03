fab21910b0472becd80b01d433c2bcedf3484d02
Blockbuster2398 <justinmcduffey@gmail.com>
2026-06-03 10:35:36 -0500

    WallFloorRatio complete

--- src/tools/metricCalculation/calculateMetrics.java
+++ src/tools/metricCalculation/calculateMetrics.java
@@ -18,6 +18,7 @@ public class calculateMetrics {
         levelMetrics.addProperty("NegativeSpace", NegativeSpace.calculateMetric(levelText));
         levelMetrics.addProperty("ShannonEntropy", ShannnonEntropy.calculateMetric(levelText));
         levelMetrics.addProperty("FloodReachability", FloodReachability.calculateMetric(levelText));
+        levelMetrics.addProperty("WallFloorRation", WallFloorRatio.calculateMetric(levelText));
         //levelMetrics.addProperty("SomeMetric", SomeMetric.calculateSomeMetric(levelText));
 
         return levelMetrics;
