a3c9a6598847e22896fc4165af748d4b0a9cda89
Justin M <justinmcduffey@gmail.com>
2026-06-04 04:42:22 -0500

    ERA start

--- metric_analysis/create_ERA_graph.py
+++ metric_analysis/create_ERA_graph.py
@@ -1,9 +1,41 @@
-from tools import create_attribute_dict
+from matplotlib import pyplot as plt
+import numpy as np
+
+from metric_analysis.tools import create_attribute_dict
 
 def create_graph(metric_tuple: tuple, json_path: str):
-    pass
+    dict = create_attribute_dict(metric_path)
+    
+    plt.style.use('_mpl-gallery-nogrid')
+
+    listX = []
+    listY = []
+    for level_path, metrics in dict.items():
+        # Create lists of the variables used in this graph
+        listX.append(metrics[selected_metrics[0]])
+        listY.append(metrics[selected_metrics[1]])
+
+    print(len(listX))
+    print(len(listY))
+
+    X = np.array(listX)
+    Y = np.array(listY)
+    
+    fig, ax = plt.subplots()
+
+
+    # ax.hist2d(X, Y, bins=(np.arange(0, 1, 0.01), np.arange(0, 1, 0.01)))
+    ax.hist2d(X, Y, bins=(50))
+
+
+    ax.set(xlim=(0, 1), ylim=(0, 1))
+
+    plt.show()
 
 
 
 metric_path = "generatedExamples/geminiLevelGenerator/metrics.json"
-selected_metrics = ("Density", "FloodReachability")
+metric_path = "generatedExamples/LocalLanguageModelGenerator/metrics.json"
+selected_metrics = ("Density", "NegativeSpace")
+
+create_graph(selected_metrics, metric_path)
--- metric_analysis/tools.py
+++ metric_analysis/tools.py
@@ -2,16 +2,16 @@
 import json
 
 
-def createAttributeDict(jsonPath : str):
+def create_attribute_dict(jsonPath : str):
     with open(jsonPath) as f:
         string = f.read()
         data = json.loads(string)
         # print(data)
-        print(data["generatedExamples\\geminiLevelGenerator\\zelda\\zelda_lvl027.txt"])
-
+        # print(data["generatedExamples\\geminiLevelGenerator\\zelda\\zelda_lvl027.txt"])
+        return data
 
 
 
 
 sample_path = "generatedExamples/geminiLevelGenerator/metrics.json"
-createAttributeDict(sample_path)
+create_attribute_dict(sample_path)
