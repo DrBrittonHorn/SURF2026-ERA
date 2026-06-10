2def50059526fc2bf79508f7cc646a2f1a8a273a
Justin Rodriguez <rodrigujus384@trinity.edu>
2026-06-10 11:48:08 -0500

    added method to metricTools, finished EnemyCount + Density, started Position

---
+++ src/tools/metricCalculation/metrics/Position.java
@@ -0,0 +1,17 @@
+package tools.metricCalculation.metrics;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class Position {
+
+    public static void calculateMetric(String LevelPath) {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
