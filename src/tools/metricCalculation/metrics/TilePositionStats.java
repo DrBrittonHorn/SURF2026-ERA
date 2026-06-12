850a8de1c1217f46e813a1a3b9d27414075c41c6
Justin Rodriguez <rodrigujus384@trinity.edu>
2026-06-12 11:07:26 -0500

    set up Symmetry and TilePositionsStats

---
+++ src/tools/metricCalculation/metrics/TilePositionStats.java
@@ -0,0 +1,30 @@
+package tools.metricCalculation.metrics;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TilePositionStats {
+    
+    /*
+    For each of the tiles we get:
+    • µx and σx - the mean and standard deviation x position
+    of that tile type.
+    • µy and σy - the mean and standard deviation y position of
+    that tile type.
+    */
+
+    public static HashMap<Character, ArrayList<Double>> calculateMetric(String LevelText) {
+        HashMap<Character, ArrayList<Double>> TileStats = new HashMap<>();
+
+
+        return TileStats;
+    }
+
+    public static void main(String[] args) throws IOException {
+        //String testLevel = Files.readString(Path.of("generatedExamples/constructiveLevelGenerator/zelda/zelda_lvl000.txt"));
+        //System.out.println("Avatar Position is " + String.valueOf(calculateMetric(testLevel)));
+    }
+}
