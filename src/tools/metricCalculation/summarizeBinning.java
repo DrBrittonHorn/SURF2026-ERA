5a63f9c660708ea42289c6ea11327c7e4cda7ad0
2025-08-20 <mcduffejus396@europa.cs.trinity.edu>
2026-07-03 16:55:18 -0500

    genetic parallelization start

---
+++ src/tools/metricCalculation/summarizeBinning.java
@@ -0,0 +1,23 @@
+package tools.metricCalculation;
+
+public class summarizeBinning {
+    public static void main(String args[]){
+        //Summarizes level validity for each generator
+
+        String[] generators = {
+            "claudeLevelGenerator",
+            "constructiveLevelGenerator",
+            "enhancedClaudeGenerator",
+            "FineTunedLLMGenerator",
+            "geminiLevelGenerator",
+            "geneticLevelGenerator",
+            "localLanguageModelGenerator",
+            "randomLevelGenerator",
+            "sturgeonLevelGenerator1x1",
+            "sturgeonLevelGenerator2x2",
+            "sturgeonLevelGenerator4x4"
+        };
+
+
+    }
+}
---
+++ src/tracks/levelGeneration/generateGeneticLevels.java
@@ -0,0 +1,15 @@
+package tracks.levelGeneration;
+
+public class generateGeneticLevels {
+    public static void main(String[] args){
+        // provide game file then output level file
+
+        String gameFile = "examples/selectedGameFiles/aliens";
+
+        // Takes in a file path that needs to have a genetic level written to it and write the level to it
+        String selectedGenerator = "tracks.levelGeneration.geneticLevelGenerator.LevelGenerator";
+        
+        
+        System.out.println("f");
+    }
+}
