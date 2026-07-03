1f2334115a9f379968b645a97a57c8bd3baacd19
2025-08-20 <mcduffejus396@europa.cs.trinity.edu>
2026-07-03 00:05:20 -0500

    finalized metrics for several generators

---
+++ src/tools/metricCalculation/cleanFinalMetrics.java
@@ -0,0 +1,24 @@
+package tools.metricCalculation;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class cleanFinalMetrics {
+    public static void main(String[] args) throws IOException{
+        Stream<Path> files = Files.walk(Path.of("finalizedMetrics"));
+
+        files.forEach(file -> {
+            if (file.toString().endsWith(".txt")){
+                try {
+                    System.out.println(file.toString());
+                    Files.deleteIfExists(file);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    
+}
