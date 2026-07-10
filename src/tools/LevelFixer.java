42580f7f60068e2d44e208db0493870d12502da9
Justin Rodriguez <rodrigujus384@trinity.edu>
2026-07-10 12:23:33 -0500

    LevelFixer tool to hopefully be used (WIP)

---
+++ src/tools/LevelFixer.java
@@ -0,0 +1,65 @@
+package tools;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class LevelFixer {
+    
+    /**
+     * replace instances of 'B' with 'A' in frogs levels
+     * @param generator the desired generator of which the frogs levels need to be fixed
+     * @throws IOException 
+     */
+    public static void FixFrogs(String generator) throws IOException {
+        String LevelMapping = Files.readString(Path.of("examples/gridphysics/frogs_glvl.txt")).split("wwwwwwwwwwwwwwwww")[0];
+        String path = null;
+        String LevelDescription = null;
+        int count = 0;
+        ArrayList<Integer> levels = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            path = ("generatedExamples\\" + generator + "\\" + "frogs" + "\\" + "frogs_lvl00" + i + ".txt");
+            LevelDescription = Files.readString(Path.of(path)).split("LevelDescription")[1].strip();
+            if (LevelDescription.contains("B")) {
+                String newLevelDescription = LevelDescription.replace("B", "A");
+                Files.writeString(Path.of(path), (LevelMapping + newLevelDescription));
+                count++;
+                levels.add(i);
+            };
+        }
+        for (int i = 10; i < 100; i++) {
+            path = ("generatedExamples\\" + generator + "\\" + "frogs" + "\\" + "frogs_lvl0" + i + ".txt");
+            LevelDescription = Files.readString(Path.of(path)).split("LevelDescription")[1].strip();
+            if (LevelDescription.contains("B")) {
+                String newLevelDescription = LevelDescription.replace("B", "A");
+                Files.writeString(Path.of(path), (LevelMapping + newLevelDescription));
+                count++;
+                levels.add(i);
+            };
+        }
+        for (int i = 100; i < 1000; i++) {
+            path = ("generatedExamples\\" + generator + "\\" + "frogs" + "\\" + "frogs_lvl" + i + ".txt");
+            LevelDescription = Files.readString(Path.of(path)).split("LevelDescription")[1].strip();
+            if (LevelDescription.contains("B")) {
+                String newLevelDescription = LevelDescription.replace("B", "A");
+                Files.writeString(Path.of(path), (LevelMapping + newLevelDescription));
+                count++;
+                levels.add(i);
+            };
+        }
+
+        System.out.println("number of fixed levels: " + count);
+        System.out.println("list of fixed levels: " + levels);
+    }
+
+    public static void FixAsteroids(String generator) {
+        
+    }
+
+    public static void main(String[] args) throws IOException {
+        FixFrogs("randomLevelGenerator");
+        FixFrogs("constructiveLevelGenerator");
+        FixFrogs("geneticLevelGenerator");
+    }
+}
